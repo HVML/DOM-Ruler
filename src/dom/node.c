c2aee48f2786f6044a822f3850d4ef023ac28da5
xueshuming <xueshuming@minigui.org>
2021-03-14 15:49:17 +0800

    add node.c

--- src/dom/node.c
+++ src/dom/node.c
@@ -45,54 +45,64 @@
  \endverbatim
  */
 
+#include "node.h"
 
-#ifndef _NODE_H_
-#define _NODE_H_
-
-typedef enum {
-    DOM_ELEMENT_NODE        = 1,
-    DOM_ATTRIBUTE_NODE      = 2,
-    DOM_TEXT_NODE           = 3,
-    DOM_CDATA_SECTION_NODE      = 4,
-    DOM_ENTITY_REFERENCE_NODE   = 5,
-    DOM_ENTITY_NODE         = 6,
-    DOM_PROCESSING_INSTRUCTION_NODE = 7,
-    DOM_COMMENT_NODE        = 8,
-    DOM_DOCUMENT_NODE       = 9,
-    DOM_DOCUMENT_TYPE_NODE      = 10,
-    DOM_DOCUMENT_FRAGMENT_NODE  = 11,
-    DOM_NOTATION_NODE       = 12,
-
-    /* And a count of the number of node types */
-    DOM_NODE_TYPE_COUNT
-} DomNodeType;
-
-typedef struct DomNode_ {
-    struct DomNode_ *parent;  /**< Parent node */
-    struct DomNode_ *first_child; /**< First child node */
-    struct DomNode_ *last_child;  /**< Last child node */
-    struct DomNode_ *previous;    /**< Previous sibling */
-    struct DomNode_ *next;        /**< Next sibling */
-
-    void *attach;
-
-    char *name;
-    char *value;
-    DomNodeType domType;
-
-    char *id;
-    char **classes;
-    int classesCount;
-    char *inlineStyle;
-
-    char *runnerType;
-} DomNode;
+#include <stdlib.h>
+#include <string.h>
 
 
 DomNode* createDomNode(char* name, char* value, DomNodeType domType, 
-        char *id, char **classes, int classesCount, char *inlineStyle, char *runnerType);
-int attachDomNode(DomNode *node, DomNode *parent, DomNode *previous, DomNode *next);
-int attachDomNoderange(DomNode *first, DomNode *last, DomNode *parent, DomNode *previous, DomNode *next);
-void destroyDomNode(DomNode *node);
+        char *id, char **classes, int classesCount, char *inlineStyle, char *runnerType)
+{
+    DomNode* result = (DomNode*)malloc(sizeof(DomNode));
+    if (result == NULL)
+    {
+        return NULL;
+    }
 
-#endif // _NODE_H_
+    if (name != NULL)
+        result->name = strdup(name);
+    else
+        result->name = NULL;
+
+    if (value != NULL)
+        result->value = strdup(value);
+    else
+        result->value = NULL;
+
+    result->domType = domType;
+
+    if (id != NULL)
+        result->id = strdup(id);
+    else
+        result->id = NULL;
+
+    if (classesCount > 0 && classes != NULL)
+    {
+        char** resultClasses = (char**) malloc(sizeof(char*)*classesCount);
+        for (int i = 0; i < classesCount; i++)
+        {
+            resultClasses[i] = strdup(classes[i]);
+        }
+        result->classes = resultClasses;
+        result->classesCount = classesCount;
+    }
+    else
+    {
+        result->classes = NULL;
+        result->classesCount = 0;
+    }
+
+    if (inlineStyle != NULL)
+        result->inlineStyle = strdup(inlineStyle);
+    else
+        result->inlineStyle = NULL;
+
+    if (runnerType != NULL)
+        result->runnerType = strdup(runnerType);
+    else
+        result->runnerType = NULL;
+
+    return result;
+}
+
