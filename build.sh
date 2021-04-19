e153fb39d19b2633240f9c6cebf20df3730625c4
xueshuming <xueshuming@minigui.org>
2021-04-19 15:11:52 +0800

    install dir /usr -> /usr/local

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ then
     BASE_DIR=$(dirname $(readlink -f "$0"))
     OUTPUT_DIR=$BASE_DIR/output
     ROOT_DIR=$OUTPUT_DIR/root
-    export CMAKE_INCLUDE_PATH="$ROOT_DIR/usr/include"
-    export CMAKE_LIBRARY_PATH="$ROOT_DIR/usr/lib"
-    export PKG_CONFIG_PATH="$ROOT_DIR/usr/lib/pkgconfig"
+    export CMAKE_INCLUDE_PATH="$ROOT_DIR/usr/local/include"
+    export CMAKE_LIBRARY_PATH="$ROOT_DIR/usr/local/lib"
+    export PKG_CONFIG_PATH="$ROOT_DIR/usr/local/lib/pkgconfig"
     mkdir -p $ROOT_DIR
 fi
 
@@ -15,7 +15,7 @@ echo ""
 echo "ROOT_DIR=$ROOT_DIR"
 echo ""
 
-cmake -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/usr
+cmake -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/usr/local
 #make VERBOSE=1
 make
 
