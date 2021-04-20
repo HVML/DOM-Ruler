0da2bd89d015d09860311a6e15b4ddc771ada888
xueshuming <xueshuming@minigui.org>
2021-04-20 10:56:51 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-if [ -z ${ROOT_DIR+x} ]; 
-then 
-    BASE_DIR=$(dirname $(readlink -f "$0"))
-    OUTPUT_DIR=$BASE_DIR/output
-    ROOT_DIR=$OUTPUT_DIR/root
-    export CMAKE_INCLUDE_PATH="$ROOT_DIR/usr/local/include"
-    export CMAKE_LIBRARY_PATH="$ROOT_DIR/usr/local/lib"
-    export PKG_CONFIG_PATH="$ROOT_DIR/usr/local/lib/pkgconfig"
-    mkdir -p $ROOT_DIR
-fi
-
-echo ""
-echo "ROOT_DIR=$ROOT_DIR"
-echo ""
-
-cmake -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/usr/local
+cmake .
 #make VERBOSE=1
 make
 
