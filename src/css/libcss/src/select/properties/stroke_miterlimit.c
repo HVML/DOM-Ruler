719fa6aeba6a33eb71ea2948b3f160b8451e1575
xueshuming <xueshuming@minigui.org>
2021-04-24 16:53:33 +0800

    update for stroke_miterlimit

--- src/css/libcss/src/select/properties/stroke_miterlimit.c
+++ src/css/libcss/src/select/properties/stroke_miterlimit.c
@@ -1,5 +1,8 @@
 /*
- * Copyright (C) 2021 Beijing FMSoft Technologies Co., Ltd.
+ * This file is part of LibCSS
+ * Licensed under the MIT License,
+ *		  http://www.opensource.org/licenses/mit-license.php
+ * Copyright 2017 Lucas Neves <lcneves@gmail.com>
  */
 
 #include "bytecode/bytecode.h"
@@ -11,27 +14,49 @@
 #include "select/properties/properties.h"
 #include "select/properties/helpers.h"
 
-css_error css__cascade_stroke_miterlimit(uint32_t opv, css_style *style,
+css_error css__cascade_stroke_miterlimit(uint32_t opv, css_style *style, 
 		css_select_state *state)
 {
+	uint16_t value = CSS_STROKE_MITERLIMIT_INHERIT;
+	css_fixed stroke_miterlimit = 0;
+
+	if (isInherit(opv) == false) {
+		value = CSS_STROKE_MITERLIMIT_SET;
+
+		stroke_miterlimit = *((css_fixed *) style->bytecode);
+		advance_bytecode(style, sizeof(stroke_miterlimit));
+	}
+
+	if (css__outranks_existing(getOpcode(opv), isImportant(opv), state,
+			isInherit(opv))) {
+		return set_stroke_miterlimit(state->computed, value, stroke_miterlimit);
+	}
+
 	return CSS_OK;
 }
 
 css_error css__set_stroke_miterlimit_from_hint(const css_hint *hint,
 		css_computed_style *style)
 {
-	return CSS_OK;
+	return set_stroke_miterlimit(style, hint->status, hint->data.fixed);
 }
 
 css_error css__initial_stroke_miterlimit(css_select_state *state)
 {
-	return CSS_OK;
+	return set_stroke_miterlimit(state->computed, CSS_STROKE_MITERLIMIT_SET, INTTOFIX(4));
 }
 
 css_error css__compose_stroke_miterlimit(const css_computed_style *parent,
 		const css_computed_style *child,
 		css_computed_style *result)
 {
-	return CSS_OK;
+	css_fixed stroke_miterlimit = 0;
+	uint8_t type = get_stroke_miterlimit(child, &stroke_miterlimit);
+
+	if (type == CSS_STROKE_MITERLIMIT_INHERIT) {
+		type = get_stroke_miterlimit(parent, &stroke_miterlimit);
+	}
+
+	return set_stroke_miterlimit(result, type, stroke_miterlimit);
 }
 
