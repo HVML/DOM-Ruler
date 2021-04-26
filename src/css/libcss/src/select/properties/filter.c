945863c3031eb8807def2576cbf8431e75b789da
xueshuming <xueshuming@minigui.org>
2021-04-26 20:18:09 +0800

    update for filter computed

--- src/css/libcss/src/select/properties/filter.c
+++ src/css/libcss/src/select/properties/filter.c
@@ -14,24 +14,38 @@
 css_error css__cascade_filter(uint32_t opv, css_style *style,
 		css_select_state *state)
 {
-	return CSS_OK;
+	return css__cascade_uri_none(opv, style, state, set_filter);
 }
 
 css_error css__set_filter_from_hint(const css_hint *hint,
 		css_computed_style *style)
 {
-	return CSS_OK;
+	css_error error;
+
+	error = set_filter(style, hint->status, hint->data.string);
+
+	if (hint->data.string != NULL)
+		lwc_string_unref(hint->data.string);
+
+	return error;
 }
 
 css_error css__initial_filter(css_select_state *state)
 {
-	return CSS_OK;
+	return set_filter(state->computed, CSS_FILTER_NONE, NULL);
 }
 
 css_error css__compose_filter(const css_computed_style *parent,
 		const css_computed_style *child,
 		css_computed_style *result)
 {
-	return CSS_OK;
+	lwc_string *url;
+	uint8_t type = get_filter(child, &url);
+
+	if (type == CSS_BACKGROUND_IMAGE_INHERIT) {
+		type = get_filter(parent, &url);
+	}
+
+	return set_filter(result, type, url);
 }
 
