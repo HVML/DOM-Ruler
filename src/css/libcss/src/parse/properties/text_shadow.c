5f431bb0c235937696d67b9784474aa2609ad543
xueshuming <xueshuming@minigui.org>
2021-04-26 19:25:30 +0800

    update for text_shadow

--- src/css/libcss/src/parse/properties/text_shadow.c
+++ src/css/libcss/src/parse/properties/text_shadow.c
@@ -11,6 +11,7 @@
 #include "parse/properties/properties.h"
 #include "parse/properties/utils.h"
 
+#define LENGTH_SIZE  3
 css_error css__parse_text_shadow_impl(css_language *c,
 		const parserutils_vector *vector, int *ctx,
 		css_style *result, int np)
@@ -22,23 +23,16 @@ css_error css__parse_text_shadow_impl(css_language *c,
 
     int last_ctx = *ctx;
 
-    css_fixed text_shadow_h;
-    uint32_t  text_shadow_h_unit;
+    css_fixed lengths[LENGTH_SIZE];
+    uint32_t  units[LENGTH_SIZE];
+    uint8_t count = 0;
 
-    css_fixed text_shadow_v;
-    uint32_t  text_shadow_v_unit;
-
-    css_fixed text_shadow_blur;
-    uint32_t  text_shadow_blur_unit;
-
-	uint32_t text_shadow_color = 0;
-	uint16_t text_shadow_color_value= 0;
-
-    uint8_t text_shadow_type = CSS_TEXT_SHADOW_INHERIT;
-    uint8_t try_parse_color = true;
+    bool has_color = false;
+    uint16_t color_value = 0;
+    uint32_t color_color = 0;
 
 	token = parserutils_vector_iterate(vector, ctx);
-	if ((token == NULL) || (token->type != CSS_TOKEN_IDENT) ) {
+	if (token == NULL) {
 		*ctx = orig_ctx;
 		return CSS_INVALID;
 	}
@@ -48,115 +42,115 @@ css_error css__parse_text_shadow_impl(css_language *c,
 	} else if (lwc_string_caseless_isequal(token->idata, c->strings[NONE], &match) == lwc_error_ok && match) {
 		error = css__stylesheet_style_appendOPV(result, CSS_PROP_TEXT_SHADOW, 0, TEXT_SHADOW_NONE);
 	} else {
-        // text_shadow_h
-        error = css__parse_unit_specifier(c, vector, ctx, UNIT_PX, &text_shadow_h, &text_shadow_h_unit);
-        if (error != CSS_OK) 
+        *ctx = orig_ctx;
+        int last_ctx = *ctx;
+        while ((token = parserutils_vector_iterate(vector, ctx)) != NULL)
         {
-            *ctx = orig_ctx;
-            return error;
+            if (token->idata == NULL)
+            {
+                last_ctx = *ctx;
+                continue;
+            }
+
+            switch (token->type)
+            {
+                case CSS_TOKEN_IDENT:
+                case CSS_TOKEN_HASH:
+                case CSS_TOKEN_FUNCTION:
+                    if (has_color)
+                    {
+                        *ctx = orig_ctx;
+                        return CSS_INVALID;
+                    }
+                    else
+                    {
+                        error = css__parse_colour_specifier(c, vector, &last_ctx, &color_value, &color_color);
+                        if (error == CSS_OK) 
+                        {
+                            has_color = true;
+                            last_ctx = *ctx;
+                        }
+                        else
+                        {
+                            *ctx = orig_ctx;
+                            return error;
+                        }
+                    }
+                    break;
+
+                case CSS_TOKEN_NUMBER:
+                case CSS_TOKEN_PERCENTAGE:
+                case CSS_TOKEN_DIMENSION:
+                    if (count < LENGTH_SIZE)
+                    {
+                        error = css__parse_unit_specifier(c, vector, &last_ctx, UNIT_PX, &lengths[count], &units[count]);
+                        if (error == CSS_OK)
+                        {
+                            count++;
+                            last_ctx = *ctx;
+                            continue;
+                        }
+                        else
+                        {
+                            *ctx = orig_ctx;
+                            return error;
+                        }
+                    }
+                    else
+                    {
+                        *ctx = orig_ctx;
+                        return CSS_INVALID;
+                    }
+                    break;
+
+                default:
+                    *ctx = orig_ctx;
+                    return CSS_INVALID;
+            }
+
+            last_ctx = *ctx;
         }
 
-        if (text_shadow_h_unit != UNIT_PX)
+        if (count < 2)
         {
             *ctx = orig_ctx;
             return CSS_INVALID;
         }
-        text_shadow_type |= TEXT_SHADOW_H;
-    }
 
-    // text_shadow_v
-	token = parserutils_vector_iterate(vector, ctx);
-	if ((token == NULL) || (token->type != CSS_TOKEN_IDENT) ) {
-		*ctx = orig_ctx;
-		return CSS_INVALID;
-	}
-
-    error = css__parse_unit_specifier(c, vector, ctx, UNIT_PX, &text_shadow_v, &text_shadow_v_unit);
-    if (error != CSS_OK) 
-    {
-        *ctx = orig_ctx;
-        return error;
-    }
-
-    if (text_shadow_v_unit != UNIT_PX)
-    {
-        *ctx = orig_ctx;
-        return CSS_INVALID;
-    }
-    text_shadow_type |= TEXT_SHADOW_V;
-
-    // text_shadow_blur
-    // text_shadow_color
-	token = parserutils_vector_iterate(vector, ctx);
-    if (token != NULL && token->type == CSS_TOKEN_IDENT)
-    {
-        error = css__parse_unit_specifier(c, vector, ctx, UNIT_PX, &text_shadow_blur, &text_shadow_blur_unit);
-        if (error == CSS_OK) 
+        uint8_t type = TEXT_SHADOW_H | TEXT_SHADOW_V;
+        if (count > 2)
         {
-            if (text_shadow_blur_unit != UNIT_PX)
-            {
-                *ctx = orig_ctx;
-                return CSS_INVALID;
-            }
-            text_shadow_type |= TEXT_SHADOW_BLUR;
-            
-            // for text shadow color
-            token = parserutils_vector_iterate(vector, ctx);
-            if ((token == NULL) || (token->type != CSS_TOKEN_IDENT) ) {
-                *ctx = orig_ctx;
-                try_parse_color = false;
-            }
+            type = type | TEXT_SHADOW_BLUR;
         }
 
-        if (try_parse_color)
+        if (has_color)
         {
-            error = css__parse_colour_specifier(c, vector, ctx, &text_shadow_color_value, &text_shadow_color);
+            type = type | TEXT_SHADOW_COLOR;
+        }
+
+        error = css__stylesheet_style_appendOPV(result, CSS_PROP_TEXT_SHADOW, 0, type);
+        if (error != CSS_OK)
+        {
+            *ctx = orig_ctx;
+            return error;
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            error = css__stylesheet_style_vappend(result, 2, lengths[i], units[i]);
             if (error != CSS_OK) {
                 *ctx = orig_ctx;
                 return error;
             }
-            if (text_shadow_color_value == COLOR_SET)
-            {
-                text_shadow_type |= TEXT_SHADOW_COLOR;
+        }
+
+        if (has_color)
+        {
+            error = css__stylesheet_style_append(result, color_color);
+            if (error != CSS_OK) {
+                *ctx = orig_ctx;
+                return error;
             }
-        }
-    }
-
-
-    error = css__stylesheet_style_appendOPV(result, CSS_PROP_TEXT_SHADOW, 0, text_shadow_type);
-    if (error != CSS_OK)
-    {
-        *ctx = orig_ctx;
-        return error;
-    }
-
-    error = css__stylesheet_style_vappend(result, 2, text_shadow_h, text_shadow_h_unit);
-    if (error != CSS_OK) {
-        *ctx = orig_ctx;
-        return error;
-    }
-
-    error = css__stylesheet_style_vappend(result, 2, text_shadow_v, text_shadow_v_unit);
-    if (error != CSS_OK) {
-        *ctx = orig_ctx;
-        return error;
-    }
-
-    if (text_shadow_type & TEXT_SHADOW_BLUR)
-    {
-        error = css__stylesheet_style_vappend(result, 2, text_shadow_blur, text_shadow_blur_unit);
-        if (error != CSS_OK) {
-            *ctx = orig_ctx;
-            return error;
-        }
-    }
-
-    if (text_shadow_type & TEXT_SHADOW_COLOR)
-    {
-        error = css__stylesheet_style_append(result, text_shadow_color);
-        if (error != CSS_OK) {
-            *ctx = orig_ctx;
-            return error;
         }
     }
 
