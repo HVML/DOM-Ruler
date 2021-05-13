92fe9a2445ebe856a67f7edc4eb4cbfba98961fb
xueshuming <xueshuming@minigui.org>
2021-05-13 14:29:15 +0800

    upate license

--- test/node_test.c
+++ test/node_test.c
@@ -20,23 +20,23 @@
 
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+    it under the terms of the GNU Lesser General License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Lesser General License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Lesser General License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     Or,
 
     As this program is a library, any link to this program must follow
-    GNU General Public License version 3 (GPLv3). If you cannot accept
-    GPLv3, you need to be licensed from FMSoft.
+    GNU Lesser General License version 3 (LGPLv3). If you cannot accept
+    LGPLv3, you need to be licensed from FMSoft.
 
     If you have got a commercial license of this program, please use it
     under the terms and conditions of the commercial license.
